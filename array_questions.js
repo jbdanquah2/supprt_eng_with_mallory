db3a835030ae2ca1fa1babe706597605a290a8c3
John Danquah-Boateng <yjbdanquah@gmail.com>
2021-07-26 19:54:04 +0000

    log comments in output

--- array_questions.js
+++ array_questions.js
@@ -4,18 +4,24 @@ const arrayTask = (arry) => {
     if (arry.length == 0) return [];
 
     //sort the array and prints it out
+    console.log('//sorting and printing the array...');
     console.log(newArry.sort((a,b) => a - b));
 
 
     //print out duplicates
+    console.log('//printing out all duplicates...')
     for (let i = 0; i < newArry.length; i++) {
          if (newArry[i] == newArry[i + 1 ]) console.log(newArry[i]);
     }
     
     
     //print out missing numbers in sequence
+    console.log('//printing out missing numbers in sequence...')
     for (let j = 0; j <= 67; j++) {
-            if (!newArry.includes(j)) console.log(j);    
+            if (!newArry.includes(j)) {
+                if (j == 0) continue;
+                console.log(j); 
+            }   
     }
 
 }
